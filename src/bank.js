bc52028db1b72fe23fc12a0964715b37656c15bb
byteknown <byteknown@gmail.com>
2024-05-19 20:32:53 +0200

    Create bank.js

---
+++ src/bank.js
@@ -0,0 +1,29 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('../users.db');
+
+async function getBank(userId) {
+    return new Promise((resolve, reject) => {
+        db.get('SELECT balance FROM users WHERE userId = ?', [userId], (err, row) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(row ? row.balance : 0);
+            }
+        });
+    });
+}
+
+// Function to update the bank balance
+async function updateBank(userId, newBalance) {
+    return new Promise((resolve, reject) => {
+        db.run('INSERT OR REPLACE INTO users (userId, balance) VALUES (?, ?)', [userId, newBalance], (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
+module.exports = { getBank, updateBank };
